d9a5348a96ce9261c76d76d73af15beb52e585ec
Ilyes Kadji <web.kin.dev@gmail.com>
2023-08-04 02:18:13 +0200

    mobil @media

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import '../layout/NavBar.scss';
-import { Link } from 'react-router-dom';
-function NavBar() {
-    return (
-    <nav className='navbar'>
-        <div className='nav-logo'>
-            <Link to='/home'>
-            <img src='navlogo.png' alt='logo' />
-            </Link>
-        </div>
-        <ul>
-            <li><Link to='/home'>Accueil</Link></li>
-            <li><Link to='/about'>A propos</Link></li>
-        </ul>
-    </nav>
-    );
-    }
+import { Link, NavLink } from 'react-router-dom';
 
-export default NavBar
+function NavBar() {
+  return (
+    <nav className='navbar'>
+      <div className='nav-logo'>
+        <Link to='/home'>
+          <img src='navlogo.png' alt='logo' />
+        </Link>
+      </div>
+      <ul>
+        <li><NavLink exact to='/home' activeClassName='active-link'>Accueil</NavLink></li>
+        <li><NavLink exact to='/about' activeClassName='active-link'>A propos</NavLink></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
