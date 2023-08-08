a5a815a8dc7ff403fb633d6be0a7b538c06fd8b2
Ilyes Kadji <web.kin.dev@gmail.com>
2023-08-08 16:00:30 +0200

    <ul>  àmedia 576px

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -3,7 +3,11 @@ function Footer() {
     return (
     <div className='kasa-footer'>
     <img src='Footerlogo.png' alt='logo' />
-    <p>&copy; 2020 KASA. All rights reserved</p>
+    <ul>
+        <li>&copy; 2020 KASA.</li>
+        <li>All rights reserved</li>
+    </ul>
+
     </div>
     );
     }
