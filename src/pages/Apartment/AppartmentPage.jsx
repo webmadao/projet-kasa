dcf7cba5d00fb9cd0d3f7ab06695edeb16f82ed5
Ilyes Kadji <web.kin.dev@gmail.com>
2023-08-21 23:47:18 +0200

    props enlevé

--- src/pages/Apartment/AppartmentPage.jsx
+++ src/pages/Apartment/AppartmentPage.jsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import ApartDetails from '../../components/ApartDetails/ApartDetails';
-import ApartEquipements from '../../components/ApartEquipements/ApartEquipements';
-import '../../components/ApartEquipements/ApartEquipements.scss';
-
 import '../Apartment/AppartmentPage.scss';
 import '../../components/Collapse/Collapse.scss';
 import MyCollapse from '../../components/Collapse/Collapse';
@@ -10,7 +6,7 @@ import Gallery from '../../components/Gallery/Gallery';
 import ApartmentMain from '../../components/ApartmentMain/ApartmentMain';
 import { useLocation } from 'react-router-dom'
 
-function AppartmentPage(props) {
+function AppartmentPage() {
   const location = useLocation()
   const state = location.state
   console.log("location", location);
@@ -24,11 +20,11 @@ function AppartmentPage(props) {
       <div className='apartment-collapse' width="calc(100% - 20px)" >
         
           <MyCollapse className="description" title="Description" content={state.description} width="calc(100% - 20px)" />
-        
+
         
           <MyCollapse className="equipements" title="Equipements" content={state.equipments.map((equipment, index) => (
     <li key={index}>{equipment}</li>))} width="calc(100% - 20px)">
-</MyCollapse>
+        </MyCollapse>
       
       </div>
     </div>
